91b54b5ece6379b35fc01980ac8d5cd2f7c052cb
Shahrair Khan Piyal <113335861+SkPiyal@users.noreply.github.com>
2023-03-19 13:54:15 +0600

    Create trainSong.cpp

---
+++ trainSong.cpp
@@ -0,0 +1,25 @@
+#include <bits/stdc++.h>
+#include<vector>
+#include<iterator>
+#include<algorithm>
+using namespace std;
+#define ll long long int
+
+
+void solve() {
+    int n,x,k;
+    cin>>n>>x;
+    k=n/(x*3);
+    cout<<k<<endl;
+
+}
+
+int main() {
+    int t;
+    cin>>t;
+    for(int i=0;i<t;i++){
+        solve();
+    }
+
+    return 0;
+}
